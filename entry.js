864019c922739190767fc489693c378b3bf40e76
Rudolf Hersén <rudolf.hersen@schibsted.se>
2016-01-31 08:51:41 +0100

    initial commit

---
+++ entry.js
@@ -0,0 +1,36 @@
+require("./style.css");
+
+var content = require("./template.hbs");
+var trains = require("./trains.hbs");
+
+window.getDepartures = function (locationSignature) {
+    console.log('GET');
+    var request = new XMLHttpRequest();
+    request.open('GET', 'departures/' + locationSignature, true);
+
+    request.onload = function () {
+        if (this.status >= 200 && this.status < 400) {
+            console.log('Success!');
+            var data = JSON.parse(this.response);
+            console.log(data);
+            var htmlString = trains(data.RESPONSE.RESULT[0]);
+            var $trains = document.getElementById('trains');
+            if ($trains) {
+                $trains.outerHTML = htmlString;
+            } else {
+                document.getElementById('navs').insertAdjacentHTML('afterend', htmlString);
+            }
+        } else {
+            console.log('We reached our target server, but it returned an error');
+        }
+    };
+
+    request.onerror = function () {
+        console.log('There was a connection error of some sort');
+    };
+
+    request.send();
+    return false;
+};
+
+document.write(content());
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    entry: "./entry.js",
+    output: {
+        path: __dirname,
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            {test: /\.css$/, loader: "style!css"},
+            {test: /\.hbs$/, loader: "handlebars"}
+        ]
+    },
+    devServer: {
+        proxy: {
+            '/departures/*': {
+                target: 'http://localhost:8081',
+                secure: false
+            }
+        }
+    }
+};
