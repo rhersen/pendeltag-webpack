614330ef19967272e97629911101018b00ce98a7
Rudolf Hersén <rudolf.hersen@schibsted.se>
2016-02-04 22:24:51 +0100

    refactoring and highlighting clicked station name

--- entry.js
+++ entry.js
@@ -4,8 +4,22 @@ var trains = require("./trains.hbs")
 var format = require("./format")
 var ajax = require("./ajax")
 
-window.getDepartures = function (locationSignature) {
-    return ajax('departures/' + locationSignature, handleJsonResponse)
+document.querySelector('body').insertAdjacentHTML('beforeend', require("./template.hbs")())
+
+var as = document.querySelectorAll('#navs nav div')
+for (var i = 0; i < as.length; i++) {
+    as[i].addEventListener('click', handleClick)
+}
+
+function handleClick() {
+    var selected = document.querySelector('.selected')
+    if (selected)
+        selected.classList.remove('selected')
+
+    document.getElementById('navs').classList.add('inactive')
+    this.classList.add('selected')
+
+    return ajax('departures/' + this.dataset.location, handleJsonResponse)
 
     function handleJsonResponse(data) {
         var htmlString = trains(data.RESPONSE.RESULT[0].TrainAnnouncement.map(format))
@@ -17,5 +31,3 @@ window.getDepartures = function (locationSignature) {
             document.getElementById('navs').insertAdjacentHTML('afterend', htmlString)
     }
 }
-
-document.write(require("./template.hbs")())
